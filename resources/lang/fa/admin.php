1054bd9ffa0d9a59157827856065e6229bfd3adc
Mohammad Mirabdolbaghi <mirabdolbaghie@gmail.com>
2018-04-24 15:19:58 +0430

    farsi language added

---
+++ resources/lang/fa/admin.php
@@ -0,0 +1,112 @@
+<?php
+
+return [
+    'online'                => 'انلاین',
+    'login'                 => 'ورود',
+    'logout'                => 'خروج',
+    'setting'               => 'تنظیمات',
+    'name'                  => 'نام',
+    'username'              => 'نام کاربری',
+    'password'              => 'رمز عبور',
+    'password_confirmation' => 'تایید رمز',
+    'remember_me'           => 'به خاطر داشته باش',
+    'user_setting'          => 'تنظیمات کاربر',
+    'avatar'                => 'عکس',
+
+    'list'         => 'لیست',
+    'new'          => 'جدید',
+    'create'       => 'ایجاد',
+    'delete'       => 'حذف',
+    'remove'       => 'حذف',
+    'edit'         => 'ویرایش',
+    'view'         => 'نمایش',
+    'browse'       => 'نشان دادن',
+    'reset'        => 'مرتب سازی مجدد',
+    'export'       => 'Export',
+    'batch_delete' => 'حذف دسته ای',
+    'save'         => 'ذخیره',
+    'refresh'      => 'رفرش',
+    'order'        => 'ترتیب',
+    'expand'       => 'باز کردن',
+    'collapse'     => 'بستن',
+    'filter'       => 'فیلتر',
+    'close'        => 'بستن',
+    'show'         => 'نمایش',
+    'entries'      => 'ردیف',
+    'captcha'      => 'Captcha',
+
+    'action'            => 'اعمال ها',
+    'title'             => 'عنوان',
+    'description'       => 'توضیحات',
+    'back'              => 'برگشت',
+    'back_to_list'      => 'برگشت به لیست',
+    'submit'            => 'فرستادن',
+    'menu'              => 'منو',
+    'input'             => 'ورودی',
+    'succeeded'         => 'موفق',
+    'failed'            => 'ناموفق',
+    'delete_confirm'    => 'مطمئن هستید از حذف فایل؟',
+    'delete_succeeded'  => 'حذف شد!',
+    'delete_failed'     => 'حذف ناموفق!',
+    'update_succeeded'  => 'ویرایش با موفقیت انجام شد!',
+    'save_succeeded'    => 'ذخیره با موفقیت انجام شد!',
+    'refresh_succeeded' => 'رفرش موفق بود!',
+    'login_successful'  => 'ورود موفق',
+
+    'choose'       => 'انتخاب کنید',
+    'choose_file'  => 'انتخاب فایل',
+    'choose_image' => 'انتخاب عکس',
+
+    'more' => 'بیشتر',
+    'deny' => 'دسترسی ندارید',
+
+    'administrator' => 'ادمین',
+    'roles'         => 'نقش ها',
+    'permissions'   => 'دسترسی ها',
+    'slug'          => 'Slug',
+
+    'created_at' => 'تاریخ ساخت',
+    'updated_at' => 'Updated At',
+
+    'alert' => 'هشدار',
+
+    'parent_id' => 'پدر',
+    'icon'      => 'ایکون',
+    'uri'       => 'URI',
+
+    'operation_log'       => ' گزارش اعمال',
+    'parent_select_error' => 'اشتباه در انتخاب پدر',
+
+    'pagination' => [
+        'range' => 'نمایش :first تا :last از :total ورودی ها',
+    ],
+
+    'role'       => 'نقش',
+    'permission' => 'دسترسی',
+    'route'      => 'مسیر',
+    'confirm'    => 'تایید',
+    'cancel'     => 'لغو',
+
+    'http' => [
+        'method' => 'HTTP method',
+        'path'   => 'HTTP path',
+    ],
+    'all_methods_if_empty' => 'تمام متد ها اگر خالی باشند',
+
+    'all'           => 'همه',
+    'current_page'  => 'صفحه فعلی',
+    'selected_rows' => 'ردیف های انتخاب شده',
+
+    'upload'     => 'اپلود',
+    'new_folder' => 'فولدر جدید',
+    'time'       => 'زمان',
+    'size'       => 'اندازه',
+
+    'listbox' => [
+        'text_total'         => 'نمایش همه {0}',
+        'text_empty'         => 'لیست خالی',
+        'filtered'           => '{0} / {1}',
+        'filter_clear'       => 'نمایش همه',
+        'filter_placeholder' => 'فیلتر',
+    ],
+];
