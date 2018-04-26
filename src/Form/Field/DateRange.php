aefcc526626b1ae57788957c8d915626deb1834d
Mohammad Mirabdolbaghi <mirabdolbaghie@gmail.com>
2018-04-26 17:09:36 +0430

    jalili Datepicker

--- src/Form/Field/DateRange.php
+++ src/Form/Field/DateRange.php
@@ -7,12 +7,13 @@ use Encore\Admin\Form\Field;
 class DateRange extends Field
 {
     protected static $css = [
-        '/vendor/laravel-admin/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css',
+        '/vendor/laravel-admin/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datepicker.min.css',
     ];
 
     protected static $js = [
         '/vendor/laravel-admin/moment/min/moment-with-locales.min.js',
-        '/vendor/laravel-admin/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js',
+        '/vendor/laravel-admin/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datepicker.min.js',
+        '/vendor/laravel-admin/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datepicker.fa.min.js',
     ];
 
     protected $format = 'YYYY-MM-DD';
@@ -55,8 +56,8 @@ class DateRange extends Field
         $class = $this->getElementClassSelector();
 
         $this->script = <<<EOT
-            $('{$class['start']}').datetimepicker($startOptions);
-            $('{$class['end']}').datetimepicker($endOptions);
+            $('{$class['start']}').datepicker($startOptions);
+            $('{$class['end']}').datepicker($endOptions);
             $("{$class['start']}").on("dp.change", function (e) {
                 $('{$class['end']}').data("DateTimePicker").minDate(e.date);
             });
